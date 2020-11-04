f3fd8ccde2dfc8843c5416ae02c22067fe4d9860
tpeyrolnik <71787776+tpeyrolnik@users.noreply.github.com>
2020-11-04 13:48:02 +0300

    Update solution_4_2.py

--- task_4/solution_4_2.py
+++ task_4/solution_4_2.py
@@ -1,6 +1,20 @@
-# your code here
-for smth in 'a'*10:
-    user_input = input()
-    # your code here
-    if my_cool_condition == True:
-        print(form, lemma)
+for i in 's'*10:
+ string = input ()
+ tab_count = 0
+ form = ''
+ lemma = ''
+ if string != '' and string[0] != '#':
+  for a in string:
+   if a == '\t':
+    tab_count += 1
+   if tab_count == 1:
+    form += a
+   if tab_count == 2:
+    lemma += a
+ k = 0
+ for letter in lemma:
+  if lemma[k] == form[k]:
+   k +=1
+  else:
+   print (form, lemma)
+   break
